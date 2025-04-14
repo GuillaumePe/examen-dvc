9718ba7202c8b48060f52f58fc2b32d1710a85ee
Ubuntu <ubuntu@ip-172-31-34-235.eu-west-1.compute.internal>
2025-04-14 12:54:22 +0000

    constructuion de la dernière étape du pipline dvc et corrections de coquilles

--- src/models/5_Evaluation_modele.py
+++ src/models/5_Evaluation_modele.py
@@ -6,21 +6,23 @@ from pathlib import Path
 import joblib
 import json
 
-project_dir = Path(__file__).resolve().parents[2]
+project_path = Path(__file__).parent.parent.parent
 
-X_test = pd.read_csv(os.path.join(project_dir,"/data/processed/X_test.csv"))
-Y_test = pd.read_csv(os.path.join(project_dir,"/data/processed/y_test.csv"))
+X_test = pd.read_csv(os.path.join(project_path,"data/standardization/X_test.csv"))
+Y_train = pd.read_csv(os.path.join(project_path,"data/processed_data/y_train.csv"))
+Y_test = pd.read_csv(os.path.join(project_path,"data/processed_data/y_test.csv"))
 Y_test = np.ravel(Y_test)
 
-model = joblib.load(os.path.join(project_dir,"models/gradient_boosting_best_model.pkl"))
+model = joblib.load(os.path.join(project_path,"models/gradient_boosting_best_model.pkl"))
 predictions = model.predict(X_test)
-
-predictions.to_csv(os.path.join(project_dir,"/data/Predictions.csv"), index=False)
+predictions = pd.DataFrame(predictions, columns=Y_train.columns)
+predictions.to_csv(os.path.join(project_path,"data/Predictions.csv"), index=False)
 
 mean_squared_error_score = mean_squared_error(Y_test,predictions)
 r2 = r2_score(Y_test, predictions)
 
+
 metrics = {"mean_squared_error": mean_squared_error_score,"r2": r2}
-metrics_path = os.path.join(project_dir,"metrics/scores.json")
+metrics_path = project_path / "metrics/scores.json"
 metrics_path.write_text(json.dumps(metrics))
 
