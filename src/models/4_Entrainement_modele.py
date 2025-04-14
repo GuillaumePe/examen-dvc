629975e43d3f75536588178a56bbc011c7cd09fc
Ubuntu <ubuntu@ip-172-31-34-235.eu-west-1.compute.internal>
2025-04-14 11:48:47 +0000

    création de la 4ème étape du pipline dvc

--- src/models/4_Entrainement_modele.py
+++ src/models/4_Entrainement_modele.py
@@ -3,12 +3,13 @@ from sklearn.ensemble import GradientBoostingRegressor
 import os
 from pathlib import Path
 import joblib
+import numpy as np
 
 project_dir = Path(__file__).resolve().parents[2]
 
-X_train = pd.read_csv(os.path.join(project_dir,"/data/processed/X_train.csv"))
-Y_train = pd.read_csv(os.path.join(project_dir,"/data/processed/y_train.csv"))
-
+X_train = pd.read_csv(os.path.join(project_dir,"data/standardization/X_train.csv"))
+Y_train = pd.read_csv(os.path.join(project_dir,"data/processed_data/y_train.csv"))
+Y_train = np.ravel(Y_train)
 best_params = joblib.load(os.path.join(project_dir,"models/gradient_boosting_best_params.pkl"))
 
 model = GradientBoostingRegressor(**best_params)
